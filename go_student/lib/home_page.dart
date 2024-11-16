be6e475043f76d13ebb9fd2edf2ed2126ffbc813
delbertkimbi <delbertdrums5@gmail.com>
2024-11-16 23:50:45 +0100

    Completed home page

--- go_student/lib/home_page.dart
+++ go_student/lib/home_page.dart
@@ -24,6 +24,7 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 20,
+                fontWeight: FontWeight.w500,
               ),
             ),
             Text(
@@ -31,6 +32,7 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 15,
+                fontWeight: FontWeight.w500,
               ),
             )
           ],
@@ -46,91 +48,99 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: const Padding(
-        padding:  EdgeInsets.all(12.0),
+        padding: EdgeInsets.all(12.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-             Card(
+            Card(
               color: Colors.white,
-              elevation: 0.5,
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: AssetImage('assets/okay.jpeg'),
-                    backgroundColor: Colors.blue,
-                    radius: 50,
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Delbert Kimbi',
-                          style: TextStyle(
-                            color: Colors.blue,
-                            fontSize: 18,
-                          ),
-                        ),
-                        Text(
-                          'FE23AXXX',
-                          style: TextStyle(
-                            color: Colors.blue,
-                            fontSize: 13,
-                          ),
-                        ),
-                        Text(
-                          'Beng Computer Engineering',
-                          style: TextStyle(
-                            color: Colors.blue,
-                            fontSize: 13,
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Text('2024/2025'),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Text('First Semester'),
-                          ],
-                        )
-                      ],
+              elevation: 0,
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/ub.jpg'),
+                      backgroundColor: Colors.blue,
+                      radius: 50,
                     ),
-                  )
-                ],
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Delbert Kimbi',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            'FE23A129',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 13,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            'B.ENG COMPUTER ENGINEERING',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 13,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text('2024/2025'),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text('First Semester'),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-           SizedBox(
+            SizedBox(
               height: 20,
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                MyBox(icon: Icons.home, name: 'Home'),
-                MyBox(icon: Icons.home, name: 'Home'),
+                MyBox(icon: Icons.grade, name: 'Academic Structure'),
+                MyBox(
+                    icon: Icons.my_library_books_rounded,
+                    name: 'Course Registration'),
               ],
             ),
-           SizedBox(
-              height: 10,
+            SizedBox(
+              height: 15,
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                MyBox(icon: Icons.home, name: 'Home'),
-                MyBox(icon: Icons.home, name: 'Home'),
+                MyBox(icon: Icons.menu_book_outlined, name: 'Form B'),
+                MyBox(icon: Icons.payments, name: 'Pay Fees'),
               ],
             ),
             SizedBox(
               height: 10,
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                MyBox(icon: Icons.home, name: 'Home'),
-                MyBox(icon: Icons.home, name: 'Home'),
+                MyBox(icon: Icons.note_add, name: 'CA Results'),
+                MyBox(icon: Icons.pages, name: 'Final Results'),
               ],
             ),
           ],
@@ -149,14 +159,15 @@ class MyBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 150,
-      width: 150,
+      width: 175,
+      padding: const EdgeInsets.all(5),
       alignment: Alignment.center,
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(10)),
       child: Column(
         children: [
           const SizedBox(
-            height: 2,
+            height: 30,
           ),
           Icon(
             icon,
@@ -167,8 +178,8 @@ class MyBox extends StatelessWidget {
             name,
             style: const TextStyle(
               color: Colors.blue,
-              fontSize: 15,
               fontWeight: FontWeight.w500,
+              fontSize: 16,
             ),
           )
         ],
