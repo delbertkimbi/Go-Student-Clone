e73e87dce9b96ed0b57cfaaecf552c693dd99e67
delbertkimbi <delbertdrums5@gmail.com>
2024-11-16 21:16:58 +0100

    Home page ui layout

---
+++ go_student/android/app/src/main/kotlin/com/example/go_student/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.go_student
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ go_student/lib/home_page.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 4,
+        centerTitle: false,
+        title: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Go-Student',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              'University of Buea',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            )
+          ],
+        ),
+        actions: const [
+          Icon(
+            Icons.more_vert,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Card(
+              color: Colors.white,
+              elevation: 0.5,
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/okay.jpeg'),
+                    backgroundColor: Colors.blue,
+                    radius: 50,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Delbert Kimbi',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          'FE23AXXX',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 13,
+                          ),
+                        ),
+                        Text(
+                          'Beng Computer Engineering',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 13,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text('2024/2025'),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text('First Semester'),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MyBox(icon: Icons.home, name: 'Home'),
+                MyBox(icon: Icons.home, name: 'Home'),
+              ],
+            ),
+           const  SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MyBox(icon: Icons.home, name: 'Home'),
+                MyBox(icon: Icons.home, name: 'Home'),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MyBox(icon: Icons.home, name: 'Home'),
+                MyBox(icon: Icons.home, name: 'Home'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyBox extends StatelessWidget {
+  IconData icon;
+  String name;
+  MyBox({super.key, required this.icon, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: 150,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 2,
+          ),
+          Icon(
+            icon,
+            color: Colors.blue,
+            size: 50,
+          ),
+          Text(
+            name,
+            style: const TextStyle(
+              color: Colors.blue,
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
